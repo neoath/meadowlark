f50843a9ca10b416b2ddbcfa810fbe9ac86d07de
lichao <lichao@worldin.cn>
2016-04-12 15:24:40 +0800

    initialize

---
+++ meadowlark.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/about', function(req, res) {
+	res.render('about');
+});
+// 404 catch-all 处理器（中间件）
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+// 500 错误处理器（中间件）
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+
+
+
+app.listen(app.get('port'), function(){
+	console.log( 'Express started on http://localhost:' +
+		app.get('port') + '; press Ctrl-C to terminate.' );
+});
